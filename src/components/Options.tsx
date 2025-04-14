ec396e94d7d1d716ff022be8cecb2e7b44486375
Priyanshu-ghub <priyanshuchoubey145@gmail.com>
2025-04-14 16:43:56 +0530

    UI Improved

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -11,13 +11,13 @@ const Options: React.FC<OptionsProps> = ({
   selectedWords,
   onSelect,
 }) => (
-  <div className="flex flex-wrap justify-center gap-2 mb-8 h-">
+  <div className="min-h-2flex flex-wrap justify-center gap-2 mb-8 h-">
     {options.map((word, i) =>
       !selectedWords.includes(word) ? (
         <button
           key={i}
           onClick={() => onSelect(word)}
-          className="px-4 py-2 rounded-lg border border-gray-300 text-gray-700 cursor-pointer hover:text-gray-800 hover:border-gray-400"
+          className=" min-h px-4 py-2 rounded-lg border border-gray-300 text-gray-700 cursor-pointer hover:text-gray-800 hover:border-gray-400"
         >
           {word}
         </button>
